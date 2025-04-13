9810ebf890fc9bde20b8abfe823bcf1291794b95
olopok <pabisa.pb@gmail.com>
2025-04-13 20:12:12 +0100

    Set up eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   eslintConfigPrettier,
-];
+]);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import "../css/style.css";
 
-function revealTheMenu(x) {
-  x.classList.toggle("visible");
-}
-
-const element = document.querySelectorAll("button[type ='menu']");
-for (let i = 0; i < element.length; i++) {
-  element[i].addEventListener("click", (e) => {
-    const firstSibling = e.target.nextElementSibling;
-    revealTheMenu(firstSibling);
-  });
-}
